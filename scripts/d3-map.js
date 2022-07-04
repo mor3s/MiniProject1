22394900d016af5e0020d75f04ae157736b0d946
Nour Boulahcen <nour.boulahcen@gmail.com>
2022-07-05 01:37:07 +0200

    viz finished

--- scripts/d3-map.js
+++ scripts/d3-map.js
@@ -1,50 +1,17 @@
 document.addEventListener("DOMContentLoaded", populateYearsOptions);
 document.addEventListener("DOMContentLoaded", mapPop);
-document.addEventListener("DOMContentLoaded", createNationalWordcloud(1900));
-// Allier Ariège Ain Hautes-Alpes Alpes-Maritimes Alpes-de-Haute-Provence Val-d'Oise Hauts-de-Seine Seine-Saint-Denis Val-de-Marne Essonne Ardennes Territoire de Belfort
-function mapBabyYear() {
-
-}
-
-function computeDistance(dept) {
-
-    var promises = [];
-    promises.push(d3.json('../data/dpt_year_parsed/dpt' + document.getElementById('year-select').value + '.json'));
-    promises.push(d3.json('../data/departement_name.json'));
-    promises.push(d3.json('../data/nat_year_parsed/nat' + document.getElementById('year-select').value + '.json'));
-    Promise.all(promises).then(function(values) {
-        let dataDept = values[0][0]["Year"];
-        let data = values[2][0]["Year"];
-        let popNat = 1
-        for (let i = 0; i < data.length; i++) {
-            popNat += parseInt(data[i].nombre)
-        }
-
-        dataDept = dataDept.filter(function(d) {
-            return d.dpt == dept;
-        });
-        let popDept = 1
-        for (let i = 0; i < dataDept.length; i++) {
-            popDept += parseInt(dataDept[i].nombre)
-        }
-        let Squaredist = 0;
-        for (let i = 0; i < data.length; i++) {
-            let deptVal = dataDept.find(x => x.preusuel == data[i].preusuel)
-            if (deptVal != undefined) {
-                Squaredist += Math.pow((parseInt(data[i].nombre) / popNat) - (parseInt(deptVal.nombre) / popDept), 2);
-            }
-
-        }
-        return Math.sqrt(Squaredist);
-    });
+document.addEventListener("DOMContentLoaded", yearChanged);
 
 
-
-}
 
 
 function yearChanged() {
     let select = document.getElementById('year-select');
+
+    let nationalSpan = document.getElementsByClassName('year')[0];
+    let deptSpan = document.getElementsByClassName('year')[1];
+    nationalSpan.innerText = select.value;
+    deptSpan.innerText = select.value;
 
     createNationalWordcloud(select.value);
     d3.select("#map").selectAll("*").remove();
@@ -52,8 +19,6 @@ function yearChanged() {
 }
 
 function populateYearsOptions() {
-    /*    <option value="1900">1900</option> ...
-    <option value="2017">2017</option>*/
     let select = document.getElementById('year-select');
     for (let i = 1900; i <= 2017; i++) {
         let option = document.createElement('option');
@@ -101,9 +66,14 @@ function mapPop() {
 
         // Quantile scales map an input domain to a discrete range, 0...max(population) to 1...9
         var quantile = d3.scaleQuantile()
-            //.domain([0, d3.max(csv, e => +e.POP)])
-            .domain([0, 0.5])
+            .domain([0.2, 0])
             .range(d3.range(9));
+
+        var legend = svg.append('text')
+            .attr('transform', 'translate(450, 125)')
+            .style("font-weight", "bold")
+            .style("font-size", "8px")
+            .text("Proximity between names at national and department scale");
 
         var legend = svg.append('g')
             .attr('transform', 'translate(525, 150)')
@@ -119,8 +89,7 @@ function mapPop() {
             .attr("class", d => "q" + d + "-9");
 
         var legendScale = d3.scaleLinear()
-            //.domain([0, d3.max(csv, e => +e.POP)])
-            .domain([0, 0.5])
+            .domain([0.2, 0])
             .range([0, 9 * 20]);
 
         var legendAxis = svg.append("g")
@@ -128,15 +97,12 @@ function mapPop() {
             .call(d3.axisRight(legendScale).ticks(6));
 
         csv.forEach(function(e, i) {
-
-
             var promises = [];
             promises.push(d3.json('../data/dpt_year_parsed/dpt' + document.getElementById('year-select').value + '.json'));
-            promises.push(d3.json('../data/departement_name.json'));
             promises.push(d3.json('../data/nat_year_parsed/nat' + document.getElementById('year-select').value + '.json'));
             Promise.all(promises).then(function(values) {
                 let dataDept = values[0][0]["Year"];
-                let data = values[2][0]["Year"];
+                let data = values[1][0]["Year"];
                 let popNat = 1
                 for (let i = 0; i < data.length; i++) {
                     popNat += parseInt(data[i].nombre)
@@ -160,7 +126,7 @@ function mapPop() {
 
                 }
                 let sqrtdist = Math.sqrt(Squaredist);
-                console.log(sqrtdist)
+                if (sqrtdist == 0) sqrtdist = 1;
                 d3.select("#d" + e.CODE_DEPT)
                     .attr("class", d => "department q" + quantile(sqrtdist) + "-9")
                     .on("mouseover", function(event, d) {
@@ -178,11 +144,6 @@ function mapPop() {
                                 "<b>Department : </b>" + e.NOM_DEPT + "<br>" +
                                 "<b>Most given name : </b>" + dataDept[0]["preusuel"] + "<br>"
                             )
-                            .style("left", (event.pageX + 30) + "px")
-                            .style("top", (event.pageY - 30) + "px");
-                        div.html("<b>Region : </b>" + e.NOM_REGION + "<br>" +
-                                "<b>Department : </b>" + e.NOM_DEPT + "<br>" +
-                                "<b>Population : </b>" + e.POP + "<br>")
                             .style("left", (event.pageX + 30) + "px")
                             .style("top", (event.pageY - 30) + "px");
                     })
@@ -212,6 +173,7 @@ function clearWordcloud(wordcloud) {
 function createNationalWordcloud(year) {
     // List of words
     clearWordcloud("#national-wordcloud");
+
     let myWords = [];
     var promises = [];
     promises.push(d3.json('../data/nat_year_parsed/nat' + year + '.json'));
@@ -226,40 +188,11 @@ function createNationalWordcloud(year) {
         for (let i = 0; i < 10; i++) {
             myWords[i] = { word: data[i]["preusuel"], size: (parseInt(data[i]["nombre"]) / pop).toString() };
         }
-        //console.log(myWords)
         createWordcloud(myWords, "national-wordcloud");
     });
-    //let myWord = [{ word: "hi", size: "10" }, { word: "bye", size: "20" }, { word: "cry", size: "50" }, { word: "shy", size: "30" }, { word: "lie", size: "20" }, { word: "chai", size: "60" }]
-    //createWordcloud(myWord, "national-wordcloud");
 }
 
-//function createDepartmentalWordcloud(dept, year) {
-//    var promises = [];
-//    let myWords = []
-//    promises.push(d3.json('../data/dpt_year_parsed/dpt' + year + '.json'));
-//    promises.push(d3.json('../data/departement_name.json'));
-//    Promise.all(promises).then(function(values) {
-//        let data = values[0][0]["Year"];
-//
-//        data = data.filter(function(d) {
-//            return d.dpt == dept;
-//        });
-//
-//        data = data.slice(0, 10);
-//        console.log(data)
-//        for (let i = 0; i < 10; i++) {
-//            myWords[i] = { text: data[i]["preusuel"], size: data[i]["nombre"], value: data[i]["nombre"] };
-//        }
-//        //console.log(myWords)
-//        createWordcloud(myWords, "dept-wordcloud");
-//    });
-//    //let myWord = [{ word: "hi", size: "10" }, { word: "bye", size: "20" }, { word: "cry", size: "50" }, { word: "shy", size: "30" }, { word: "lie", size: "20" }, { word: "chai", size: "60" }]
-//    //reateWordcloud(myWord, "dept-wordcloud");
-//
-//}
-
 function createWordcloud(myWords, id) {
-    //console.log(myWords)
     // set the dimensions and margins of the graph
     let width = 450,
         height = 450;
@@ -268,8 +201,16 @@ function createWordcloud(myWords, id) {
         .attr("width", width)
         .attr("height", height)
         .append("g")
-        .attr("transform",
-            "translate(" + 10 + "," + 10 + ")");
+        .text(function(d) {
+            if (id == "national-wordcloud") {
+                return "National";
+            } else {
+                return 0;
+            }
+        })
+
+    .attr("transform",
+        "translate(" + 10 + "," + 10 + ")");
 
     // Constructs a new cloud layout instance. It run an algorithm to find the position of words that suits your requirements
     // Wordcloud features that are different from one word to the other must be here
@@ -284,15 +225,23 @@ function createWordcloud(myWords, id) {
     // This function takes the output of 'layout' above and draw the words
     // Wordcloud features that are THE SAME from one word to the other can be here
     function draw(words) {
-        //console.log(words)
-        svg
-            .append("g")
+        let maxVal = Math.max.apply(Math, words.map(function(o) {
+            return o.size;
+        }));
+        svg.append("g")
             .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
             .selectAll("text")
             .data(words)
             .enter().append("text")
             .style("font-size", function(d) { return d.size; })
-            .style("fill", "#69b3a2")
+            .style("fill", function(d) {
+                const scale = (number, [inMin, inMax], [outMin, outMax]) => {
+                    // if you need an integer value use Math.floor or Math.ceil here
+                    return (number - inMin) / (inMax - inMin) * (outMax - outMin) + outMin;
+                }
+                s = scale(d.size, [0, maxVal], [0, 1]);
+                return d3.interpolateYlGnBu(s);
+            })
             .attr("text-anchor", "middle")
             .style("font-family", "Impact")
             .attr("transform", function(d) {
